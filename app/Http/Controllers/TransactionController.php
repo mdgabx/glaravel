a20bec3951024b675ec553700d2cd4b0d66f714f
Dex <a.markdexter@gmail.com>
2025-06-22 16:26:19 +0800

    routes

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    //
+    public function index(): string
+    {
+        return 'Transaction page';
+    }
+
+    public function show(int $transactionId): string
+    {
+        return 'Transaction' . " " . $transactionId;
+    }
+
+    public function create(): string
+    {
+        return 'Transaction create';
+    }
+
+    public function store(Request $request): string
+    {
+        return 'Transaction store';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
 use App\Enums\FileType;
+use App\Http\Controllers\ProcessTransactionController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
-Route::get('/transaction/{id}', function ($id) {
-    return $id;
-});
+// Route::get('/transaction/{id}', function ($id) {
+//     return $id;
+// });
 
 
 // Route::get('/transaction/{transactionId}/files/{fileId}', function($tid, $fid) {
@@ -26,12 +28,21 @@ Route::get('/transaction/{id}', function ($id) {
 // })->whereIn('ftype', ['receipt', 'deposit']);
 
 
-Route::get('/transaction/{transactionId}/files/{ftype}', function($tid, FileType $ftype) {
-    return $tid . " . " . " - " . ". " . $ftype->value;
-});
+// Route::get('/transaction/{transactionId}/files/{ftype}', function($tid, FileType $ftype) {
+//     return $tid . " . " . " - " . ". " . $ftype->value;
+// });
 
 
 
-Route::get('/report/{year}/{month?}', function($tid, $fid = null) {
-    return $tid . " - " . $fid;
-});
+// Route::get('/report/{year}/{month?}', function($tid, $fid = null) {
+//     return $tid . " - " . $fid;
+// });
+
+Route::get('/transactions', [TransactionController::class, 'index']);
+Route::get('/transactions/{transactionId}', [TransactionController::class, 'show'])->whereNumber('transactionId');
+Route::get('/transactions/create', [TransactionController::class, 'create']);
+Route::post('/transactions', [TransactionController::class, 'store']);
+
+// Route::get('/transactions/{transactionId}/process', [ProcessTransactionController::class, '__invoke']); 
+// or
+Route::get('/transactions/{transactionId}/process', ProcessTransactionController::class); 
